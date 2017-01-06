b29ebee3d7fa4bf5a0e6aae6538cfb1c2943943b
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-05 20:20:40 -0600

    taimen: add hardware key overlays
    Change-Id: I3c275ba340000702dc60a45a683d22465463c490
    Signed-off-by: Joe Maples <joe@frap129.org>

--- device-carbon.mk
+++ device-carbon.mk
@@ -2,5 +2,8 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.fingerprint=fpc
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/taimen/overlay-carbon
+
 # Update engine
 PRODUCT_HOST_PACKAGES += brillo_update_payload
