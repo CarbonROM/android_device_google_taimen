3d71f9893f3c9d0c25eaffa816177f1b0f78b2e4
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-19 14:59:15 -0600

    taimen: use qualcomm time services
    Change-Id: I1bce53f2e96f55bf5850b55a5dc144d9b1412b97
    Signed-off-by: Joe Maples <joe@frap129.org>

--- BoardConfigCarbon.mk
+++ BoardConfigCarbon.mk
@@ -5,3 +5,6 @@ TARGET_KERNEL_CONFIG := carbon_taimen_defconfig
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
+
+# Time services
+BOARD_USES_QC_TIME_SERVICES := true
