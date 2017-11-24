d3fac8c555391cdfe2e0b8afaa32ff061173ef76
Christian Oder <myself5@carbonrom.org>
2017-11-24 17:22:18 +0100

    taimen: Carbonify
    Signed-off-by: Joe Maples <joe@frap129.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,3 +106,5 @@ TARGET_RECOVERY_UI_MARGIN_HEIGHT := 105
 # VTS DTBO Verification. This kernel cmdline parameter should be added by the bootloader
 # for all future devices.
 BOARD_KERNEL_CMDLINE += androidboot.dtbo_idx=12
+
+-include device/google/taimen/BoardConfigCarbon.mk
