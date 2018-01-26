9c303a77db1a45219d765c19bf64832551128dc3
Christian Oder <myself5@carbonrom.org>
2018-01-26 19:34:21 +0100

    [DNM] Include prebuilt GApps
    Change-Id: Idb5703a92a70faeacfb0ddd0faf8ac0d782d39d3

--- device-carbon.mk
+++ device-carbon.mk
@@ -33,3 +33,19 @@ PRODUCT_PACKAGES += \
 # Privileged app permissions
 PRODUCT_COPY_FILES += \
     device/google/taimen/privapp-permissions-taimen.xml:system/etc/permissions/privapp-permissions-taimen.xml
+
+# Build gapps
+GAPPS_VARIANT := nano
+PRODUCT_PACKAGES += \
+    CameraGoogle \
+    PixelLauncher \
+    Search \
+    Wallpapers
+
+GAPPS_PACKAGE_OVERRIDES := \
+    CameraGoogle \
+    PixelLauncher \
+    Search \
+    Wallpapers
+
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
