3936022ac6278da203b9eb0efeb3ef0ab9131aab
Joe Maples <joe@frap129.org>
2018-09-02 15:33:16 -0400

    BoardConfig: Use SDClang
    Change-Id: I3f8ff6bb1612181679e9c1e415df19c272982b7f
    Signed-off-by: Joe Maples <joe@frap129.org>

--- BoardConfigCarbon.mk
+++ BoardConfigCarbon.mk
@@ -6,4 +6,7 @@ BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
 
+# Compile with SDClang
+TARGET_USE_SDCLANG := true
+
 -include vendor/google/taimen/BoardConfigVendor.mk
