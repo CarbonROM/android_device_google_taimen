ea477e3b045f05d9792b17e45567ab17534855b2
Christian Oder <myself5@carbonrom.org>
2018-10-07 21:34:53 +0200

    taimen: Update fingerprint to PPR2.181005.003
    Change-Id: I7fb0889e1438d1250ba0e075150e3efc8f61c9a3

--- carbon_taimen.mk
+++ carbon_taimen.mk
@@ -23,9 +23,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    PRIVATE_BUILD_DESC="taimen-user 9 PPR2.180905.005 4928864 release-keys"
+    PRIVATE_BUILD_DESC="taimen-user 9 PPR2.181005.003 4984323 release-keys"
 
-BUILD_FINGERPRINT := google/taimen/taimen:9/PPR2.180905.005/4928864:user/release-keys
+BUILD_FINGERPRINT := google/taimen/taimen:9/PPR2.181005.003/4984323:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carbon.maintainer="frap129/Myself5"
