af8b9928aeef3bf14683f70640ed5c500708c29e
Christian Oder <myself5@carbonrom.org>
2019-01-09 19:00:42 +0100

    taimen: Update fingerprint to PQ1A.190105.004
    Change-Id: I35f208d77e30ba02ce639edb10868f88117a7d30

--- carbon_taimen.mk
+++ carbon_taimen.mk
@@ -23,9 +23,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    PRIVATE_BUILD_DESC="taimen-user 9 PQ1A.181205.002 5086253 release-keys"
+    PRIVATE_BUILD_DESC="taimen-user 9 PQ1A.190105.004 5148680 release-keys"
 
-BUILD_FINGERPRINT := google/taimen/taimen:9/PQ1A.181205.002/5086253:user/release-keys
+BUILD_FINGERPRINT := google/taimen/taimen:9/PQ1A.190105.004/5148680:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.carbon.maintainer="frap129/Myself5"
