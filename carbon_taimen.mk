3c69411325296ed31728912dc608eddf22b77166
Christian Oder <myself5@carbonrom.org>
2020-10-18 16:57:12 +0200

    taimen: Add Carbon configs

--- carbon_taimen.mk
+++ carbon_taimen.mk
@@ -2,16 +2,19 @@
 TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
--include device/google/taimen/device-lineage.mk
+-include device/google/taimen/device-carbon.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_taimen
+PRODUCT_NAME := carbon_taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
 PRODUCT_RESTRICT_VENDOR_FILES := false
---
+++ device-carbon.mk
@@ -0,0 +1,2 @@
+# Wahoo common config
+-include device/google/wahoo/device-carbon.mk
